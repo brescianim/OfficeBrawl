db08f590e3de50ae7cca609e086dfe0d1561b96f
brescianim <97706677+brescianim@users.noreply.github.com>
2022-04-05 11:54:49 -0400

    Remade PhillipController

---
+++ OfficeBrawl/Assets/Scripts/PhillipController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhillipController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Vector2 movement;
+    public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // 
+    }
+}
