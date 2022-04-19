ace3974c7ee7673e08d7bd50993fcd9f01cc29ff
brescianim <97706677+brescianim@users.noreply.github.com>
2022-04-19 03:35:41 -0400

    Started coding in animations to activate on keyboard input

--- OfficeBrawl/Assets/Scripts/PhillipController.cs
+++ OfficeBrawl/Assets/Scripts/PhillipController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhillipController : MonoBehaviour
 {
@@ -8,19 +9,39 @@ public class PhillipController : MonoBehaviour
     public Vector2 movement;
     public float moveSpeed = 5f;
     public DummySettings dummy;
-
+    public Animator m_animator;
     public MenuController menuController;
+
+    private bool hasHorizontalInput;
+    private bool isWalking;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        hasHorizontalInput = !Mathf.Approximately(movement.x, 0f);
+        isWalking = hasHorizontalInput;
+        m_animator.SetBool("isWalking", isWalking);
+
+        if (isWalking)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                m_animator.Play("phillip_walk_left");
+            }
+            else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                m_animator.Play("phillip_walk_right");
+            }
+        }
+
         End();
     }
 
